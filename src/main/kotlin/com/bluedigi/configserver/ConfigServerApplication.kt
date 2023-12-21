b4192e0be42b86b3da198369368ba823a4c6dc87
EmmanuelDMV <emartinez@ahorra.io>
2023-12-21 12:02:44 -0600

    changed path for profiles

--- src/main/kotlin/com/bluedigi/configserver/ConfigServerApplication.kt
+++ src/main/kotlin/com/bluedigi/configserver/ConfigServerApplication.kt
@@ -2,6 +2,7 @@ package com.bluedigi.configserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
 @EnableConfigServer
